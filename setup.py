e74ddb92271f7552d6497aab21a2953166e7f010
Krezcs <130219080+Krezcs@users.noreply.github.com>
2023-04-16 18:41:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name="clean_folder",
-    version="0.1",
-    packages=find_packages(),
+    name='clean_folder',
+    version='0.1',
     description='script cleans folder',
+    author='Yaroslav',
     classifiers=[
-          'License '   
-          'Programming language Python 3.10'
-    ],
+            'License '   
+            'Programming language Python 3.10'
+        ],   
+    packages=find_packages(),
     python_requires='>=3.5',
-    include_package_data=True
+    include_package_data=True,    
+    entry_points={
+        'console_scripts': [
+            'clean-folder = clean_folder.clean_folder:__main__',
+        ],
+    }
 )
